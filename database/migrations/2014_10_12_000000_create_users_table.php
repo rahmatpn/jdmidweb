2d18ff8899f846c3d15d1a997372bb02ec7d5eba
hakivin <hakivinamana@student.uns.ac.id>
2020-01-14 16:26:14 +0700

    change database structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',[0,1]);//0 = hotel, 1 = user
             $table->rememberToken();
             $table->timestamps();
         });
