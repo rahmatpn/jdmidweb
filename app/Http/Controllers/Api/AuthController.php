0769273ce85b9af465773ae50d3ecf15802a85e8
hakivin <hakivinamana@student.uns.ac.id>
2020-01-14 15:08:29 +0700

    add login api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -22,4 +22,18 @@ class AuthController extends Controller
         $accessToken = $user->createToken('authToken')->accessToken;
         return response(['user'=> $user, 'access_token'=> $accessToken]);
     }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if(!auth()->attempt($loginData)) {
+            return response(['message'=>'Invalid credentials']);
+        }
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
 }
