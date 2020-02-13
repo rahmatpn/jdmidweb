0bf5c8fcde817a226c059cc8dd00213a93f281b0
maunabesanika <maunabesanika@student.uns.ac.id>
2020-02-13 10:49:01 +0700

    once again, fix auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -277,7 +277,7 @@
 
 
                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                   <a class="dropdown-item" href="{{url('/hotel/'.$hotel->profile->url_slug.'/edit')}}">Edit Profile</a>
+                                   <a class="dropdown-item" href="{{url('/hotel/'.auth()->guard('hotel')->user()->profile->url_slug.'/edit')}}">Edit Profile</a>
                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                        {{ __('Logout') }}
                                    </a>
