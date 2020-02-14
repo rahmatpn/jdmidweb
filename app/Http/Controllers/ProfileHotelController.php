5970e86109837b19ef5764bf4b22f21543708a47
maunabesanika <maunabesanika@student.uns.ac.id>
2020-02-14 11:24:23 +0700

    fix hotel image

--- app/Http/Controllers/ProfileHotelController.php
+++ app/Http/Controllers/ProfileHotelController.php
@@ -13,7 +13,8 @@ use Intervention\Image\Facades\Image;
 
 class ProfileHotelController extends Controller
 {
-    public function indexHotel($url_slug){
+    public function indexHotel($url_slug)
+    {
         $profil = ProfileHotel::where('url_slug', '=', $url_slug)->first();
         if ($profil != null) {
             $hotel = Hotel::findOrFail($profil->hotel_id);
@@ -23,44 +24,52 @@ class ProfileHotelController extends Controller
         }
     }
 
-    public function edit ($url_slug){
+    public function edit($url_slug)
+    {
         $profil = ProfileHotel::where('url_slug', '=', $url_slug)->with('hotel')->first();
         $hotel = Hotel::find($profil->hotel_id);
         return view('profile.editHotel', compact('hotel'));
     }
 
 
-    public function update($url_slug){
+    public function update($url_slug)
+    {
         $profile = ProfileHotel::where('url_slug', '=', $url_slug)->with('hotel')->first();
-
+        $hotel = Hotel::find($profile->hotel_id);
 
         $data = request()->validate([
             'nama' => 'required',
             'nomor_telepon' => '',
             'alamat' => '',
-            'social_media'=> '',
+            'social_media' => '',
             'website' => '',
-            'foto'=>''
+            'foto' => ''
         ]);
 
         $profile->update($data);
 
-        if(request('foto')){
-            $fotoPath = request('foto')->store('hotel/photo','public');
+        if (request('foto')) {
+            $fotoPath = request('foto')->store('hotel/photo', 'public');
 
-            $foto = Image::make(public_path("image/{$fotoPath}"))->fit(1000,1000);
+            $foto = Image::make(public_path("image/{$fotoPath}"))->fit(1000, 1000);
             $foto->save();
-            $fotoArray = ['foto'=> $fotoPath];
+            $path = "/image/{$fotoPath}";
+            $fotoArray = ['foto' => $path];
+            if ($hotel->profile->foto != null) {
+                $oldPath = public_path($hotel->profile->foto);
+                if (file_exists($oldPath)) { //If it exits, delete it from folder
+                    unlink($oldPath);
+                }
+            }
+
+
+            auth()->user()->profile->update(array_merge(
+                $data,
+                $fotoArray ?? []
+            ));
+
+
+            return redirect("/hotel/{$profile->url_slug}")->with("success", "Data Has Been Updated Successfully");
         }
-
-
-
-        auth()->user()->profile->update(array_merge(
-            $data,
-            $fotoArray ?? []
-        ));
-
-
-        return redirect("/hotel/{$profile->url_slug}")->with("success","Data Has Been Updated Successfully");
     }
 }
